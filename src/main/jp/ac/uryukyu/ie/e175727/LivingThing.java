1b027f0d975b42eb5dd1f5f0d6dfce3b27510c9f
Rito Mekaru <e175727@ie.u-ryukyu.ac.jp>
2018-11-06 17:37:08 +0900

    ステップ3: カプセル化しよう。

--- src/main/jp/ac/uryukyu/ie/e175727/LivingThing.java
+++ src/main/jp/ac/uryukyu/ie/e175727/LivingThing.java
@@ -1,10 +1,10 @@
 package jp.ac.uryukyu.ie.e175727;
 
 public class LivingThing {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     public LivingThing (String name, int maximumHP, int attack) {
         this.name = name;
@@ -37,4 +37,17 @@ public class LivingThing {
             System.out.printf("%sは倒れた。\n", name);
         }
     }
+
+    public int getHitPoint() {
+        return hitPoint;
+    }
+
+    public void setHitPoint(int hitPoint){
+        this.hitPoint = hitPoint;
+    }
+
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
+
 }
