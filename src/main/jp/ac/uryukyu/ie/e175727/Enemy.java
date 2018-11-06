473777620d74e571ba11ec6dd52dd48e0171cd17
Rito Mekaru <e175727@ie.u-ryukyu.ac.jp>
2018-11-06 17:32:02 +0900

    ステップ2: EnemyクラスとHeroクラスの重複をどうにかしたい。

--- src/main/jp/ac/uryukyu/ie/e175727/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e175727/Enemy.java
@@ -1,10 +1,6 @@
 package jp.ac.uryukyu.ie.e175727;
 
-public class Enemy {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
+public class Enemy extends LivingThing {
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -13,36 +9,8 @@ public class Enemy {
      * @param attack モンスターの攻撃力
      */
     public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
+       super(name, maximumHP, attack);
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    /**
-     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
-     * @return boolean
-     */
-    public boolean isDead() {
-        return dead;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        if(dead == false){
-            int damage = (int) (Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }
     }
 
     /**
@@ -50,6 +18,7 @@ public class Enemy {
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
      * @param damage 受けたダメージ
      */
+    @Override
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
